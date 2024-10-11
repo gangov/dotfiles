4d61eabc4897dea99fcc9430e5d035e9228cb7e8
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-10-11 20:58:01 +0300

    Fish
    removes ssh agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,22 +20,6 @@ function howto
 end
 #######
 
-####### SSH agent setup for Fish shell
-#if test -f "$XDG_RUNTIME_DIR/ssh-agent.env"
-#    # Read the file and set environment variables manually, skipping lines without "="
-#    for line in (cat $XDG_RUNTIME_DIR/ssh-agent.env)
-#        # Only process lines that contain '='
-#        if string match -r '^[A-Za-z_][A-Za-z0-9_]*=.*' $line
-#            set key (echo $line | cut -d '=' -f 1)
-#            set value (echo $line | cut -d '=' -f 2 | cut -d ';' -f 1)
-#
-#            # Export the variable
-#            set -x $key $value
-#        end
-#    end
-#end
-#######
-
 ####### adds starship
 starship init fish | source
 #######
