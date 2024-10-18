f628af51e54af0f159f7e06cc274b2667a7d9dd8
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-10-18 19:50:03 +0300

    Fish: ssh-agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Start SSH agent and add key if not already done
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) > /dev/null
+    ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
+end
+
 set -U fish_key_bindings fish_default_key_bindings
 set -Ux EDITOR nvim
 
